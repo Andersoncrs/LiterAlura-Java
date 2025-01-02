6ea3c033d60968a06d294584baaf149d1e39d3f3
Anderson Rodriguez <andersoncamilo.rodriguez.s@gmail.com>
2025-01-01 22:14:41 -0500

    Arreglo formato clase principal

--- src/main/java/com/andersonrodriguez/literalura/LiteraluraApplication.java
+++ src/main/java/com/andersonrodriguez/literalura/LiteraluraApplication.java
@@ -14,105 +14,105 @@ import java.util.List;
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraluraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LiteraluraApplication.class, args);
+    }
 
-	@Autowired
-	LibroService libroService;
+    @Autowired
+    LibroService libroService;
 
-	@Autowired
-	AutorService autorService;
+    @Autowired
+    AutorService autorService;
 
-	@Autowired
-	IdiomaService idiomaService;
+    @Autowired
+    IdiomaService idiomaService;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Menu menu = new Menu();
+    @Override
+    public void run(String... args) throws Exception {
+        Menu menu = new Menu();
 
-		menu.mostrarBienvenida();
+        menu.mostrarBienvenida();
 
-		cicloPrincipal:
-		while(true){
-			int opcionIngresada = menu.mostrarMenuPrincipal();
-			switch (opcionIngresada){
-				case 1 -> {
-					String busquedaTitulo = menu.mostrarMenuBusquedaTitulo();
-					String json = new ConsumoApi(busquedaTitulo).ObtenerJsonBusqueda();
-					DatosAPi datosAPi = new ConvierteDatos().obtenerDatos(json, DatosAPi.class);
-					if(datosAPi.comprobarExistenciaResultados()){
-						menu.mostrarResultadoNoEncontrado(busquedaTitulo);
-						continue cicloPrincipal;
-					}
-					List<DatosLibro> datosLibroList = datosAPi.resultados();
-					if(datosLibroList.size() > 20){
-						datosLibroList = datosLibroList.subList(0, 20);
-					}
+        cicloPrincipal:
+        while (true) {
+            int opcionIngresada = menu.mostrarMenuPrincipal();
+            switch (opcionIngresada) {
+                case 1 -> {
+                    String busquedaTitulo = menu.mostrarMenuBusquedaTitulo();
+                    String json = new ConsumoApi(busquedaTitulo).ObtenerJsonBusqueda();
+                    DatosAPi datosAPi = new ConvierteDatos().obtenerDatos(json, DatosAPi.class);
+                    if (datosAPi.comprobarExistenciaResultados()) {
+                        menu.mostrarResultadoNoEncontrado(busquedaTitulo);
+                        continue;
+                    }
+                    List<DatosLibro> datosLibroList = datosAPi.resultados();
+                    if (datosLibroList.size() > 20) {
+                        datosLibroList = datosLibroList.subList(0, 20);
+                    }
 
-					int opcionLibro = menu.mostrarLibrosEncontrados(datosLibroList);
-					if(opcionLibro == 0){
-						continue cicloPrincipal;
-					}
-					DatosLibro datosLibro = datosLibroList.get(opcionLibro - 1);
-					Libro libro = new Libro(
-							datosLibro.titulo(),
-							datosLibro.descargas(),
-							datosLibro.autores(),
-							datosLibro.idiomas()
-					);
-					libro.setAutorList(autorService.comprobarExistenciaAutores(libro));
-					libro.setIdiomaList(idiomaService.comprobarExistenciaIdiomas(libro));
-					if(libroService.comprobarExistenciaLibro(libro)){
-						menu.mostrarExistenciaLibro(libro);
-						continue cicloPrincipal;
-					}
-					libroService.AgregarLibro(libro);
-					menu.mostrarMensajeLibroGuardado(libro);
-				}
-				case 2 ->{
-					List<Libro> librosEnLaBaseDeDatos = libroService.listarTodosLosLibros();
-					if(librosEnLaBaseDeDatos.isEmpty()){
-						menu.mostrarListaVacia("Libro");
-						continue cicloPrincipal;
-					}
-					menu.mostrarLibros(librosEnLaBaseDeDatos);
-				}
-				case 3 -> {
-					List<Autor> autoresEnLaBaseDeDatos = autorService.listarTodosLosAutores();
-					if(autoresEnLaBaseDeDatos.isEmpty()){
-						menu.mostrarListaVacia("Autor");
-						continue cicloPrincipal;
-					}
-					menu.mostrarAutores(autoresEnLaBaseDeDatos);
-				}
-				case 4 ->{
-					int fechaIngresada = menu.solicitarFechaIngreso();
-					List<Autor> autorList = autorService.BuscarAutoresVivosPorFecha(fechaIngresada);
-					if(autorList.isEmpty()){
-						menu.mostrarFechaNoEncontrada(fechaIngresada);
-						continue cicloPrincipal;
-					}
-					menu.mostrarAutores(autorList);
-				}
-				case 5 -> {
-					List<Idioma> idiomasDisponibles = idiomaService.listarIdiomasDisponibles();
-					if(idiomasDisponibles.isEmpty()) {
-						menu.mostrarListaVacia("Idioma");
-						continue cicloPrincipal;
-					}
-					int idiomaSeleccionado = menu.mostrarIdiomasDisponibles(idiomasDisponibles);
-					if(idiomaSeleccionado == 0){
-						continue cicloPrincipal;
-					}
-					menu.mostrarLibros(idiomasDisponibles.get(idiomaSeleccionado - 1).getLibroList());
-				}
-				case 0 -> {
-					menu.MostrarDespedida();
-					break cicloPrincipal;
-				}
-			}
+                    int opcionLibro = menu.mostrarLibrosEncontrados(datosLibroList);
+                    if (opcionLibro == 0) {
+                        continue;
+                    }
+                    DatosLibro datosLibro = datosLibroList.get(opcionLibro - 1);
+                    Libro libro = new Libro(
+                            datosLibro.titulo(),
+                            datosLibro.descargas(),
+                            datosLibro.autores(),
+                            datosLibro.idiomas()
+                    );
+                    libro.setAutorList(autorService.comprobarExistenciaAutores(libro));
+                    libro.setIdiomaList(idiomaService.comprobarExistenciaIdiomas(libro));
+                    if (libroService.comprobarExistenciaLibro(libro)) {
+                        menu.mostrarExistenciaLibro(libro);
+                        continue;
+                    }
+                    libroService.AgregarLibro(libro);
+                    menu.mostrarMensajeLibroGuardado(libro);
+                }
+                case 2 -> {
+                    List<Libro> librosEnLaBaseDeDatos = libroService.listarTodosLosLibros();
+                    if (librosEnLaBaseDeDatos.isEmpty()) {
+                        menu.mostrarListaVacia("Libro");
+                        continue;
+                    }
+                    menu.mostrarLibros(librosEnLaBaseDeDatos);
+                }
+                case 3 -> {
+                    List<Autor> autoresEnLaBaseDeDatos = autorService.listarTodosLosAutores();
+                    if (autoresEnLaBaseDeDatos.isEmpty()) {
+                        menu.mostrarListaVacia("Autor");
+                        continue;
+                    }
+                    menu.mostrarAutores(autoresEnLaBaseDeDatos);
+                }
+                case 4 -> {
+                    int fechaIngresada = menu.solicitarFechaIngreso();
+                    List<Autor> autorList = autorService.BuscarAutoresVivosPorFecha(fechaIngresada);
+                    if (autorList.isEmpty()) {
+                        menu.mostrarFechaNoEncontrada(fechaIngresada);
+                        continue;
+                    }
+                    menu.mostrarAutores(autorList);
+                }
+                case 5 -> {
+                    List<Idioma> idiomasDisponibles = idiomaService.listarIdiomasDisponibles();
+                    if (idiomasDisponibles.isEmpty()) {
+                        menu.mostrarListaVacia("Idioma");
+                        continue;
+                    }
+                    int idiomaSeleccionado = menu.mostrarIdiomasDisponibles(idiomasDisponibles);
+                    if (idiomaSeleccionado == 0) {
+                        continue;
+                    }
+                    menu.mostrarLibros(idiomasDisponibles.get(idiomaSeleccionado - 1).getLibroList());
+                }
+                case 0 -> {
+                    menu.MostrarDespedida();
+                    break cicloPrincipal;
+                }
+            }
 
-		}
-	}
+        }
+    }
 }
